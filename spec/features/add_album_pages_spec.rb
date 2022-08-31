3fca6999854cf0f955cddd84b599b9109aef35b8
Staff <staff@epicodus-staff-8.hsd1.or.comcast.net>
2022-08-31 15:42:11 -0700

    update feature test for devise

--- spec/features/add_album_pages_spec.rb
+++ spec/features/add_album_pages_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe "the add a album process" do
+  before do
+    User.destroy_all
+    admin = User.create(:email => "admin@admin.com", :password => "adminadmin", :admin => true)
+    visit albums_path
+    click_button "Sign_in"
+    fill_in "Email", :with => 'admin@admin.com'
+    fill_in "Password", :with => 'adminadmin'
+    click_on "Log in"
+  end
+
   it "adds a new album" do
     visit albums_path
     click_button 'Create new album'
